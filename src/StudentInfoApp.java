c1738bb910cb43942925dfce3a6377dcfc07d48d
riddhi-decodes <iemriddhi2006@gmail.com>
2026-03-21 18:19:10 +0530

    Day 2: Added name input field and course dropdown

--- src/StudentInfoApp.java
+++ src/StudentInfoApp.java
@@ -1,13 +1,33 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class StudentInfoApp {
 
     private JFrame frame;
+    private JTextField nameField;
+    private JComboBox<String> courseBox;
 
     public StudentInfoApp() {
         frame = new JFrame("Student Information System");
         frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        // Name input
+        JLabel nameLabel = new JLabel("Name:");
+        nameField = new JTextField(15);
+
+        // Course dropdown
+        JLabel courseLabel = new JLabel("Course:");
+        courseBox = new JComboBox<>(new String[]{
+                "Math", "Science", "History"
+        });
+
+        // Add to frame
+        frame.add(nameLabel);
+        frame.add(nameField);
+        frame.add(courseLabel);
+        frame.add(courseBox);
 
         frame.setVisible(true);
     }
